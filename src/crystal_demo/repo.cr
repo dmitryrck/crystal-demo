70cbab43aa5901e69182b17e703977ed86bb1aa5
Dmitry Rocha <dmitryrck@gmail.com>
2017-04-07 19:58:35 -0300

    Change CrystalDemo::Repo to use uri.

--- src/crystal_demo/repo.cr
+++ src/crystal_demo/repo.cr
@@ -1,18 +1,9 @@
-require "uri"
-
 module CrystalDemo
   module Repo
     extend Crecto::Repo
 
-    database_url = URI.parse(ENV["DATABASE_URL"])
-
     config do |conf|
-      conf.adapter = Crecto::Adapters::Postgres
-      conf.database = database_url.path.as(String).gsub("/", "")
-      conf.hostname = database_url.host || "127.0.0.1"
-      conf.username = database_url.user || "postgres"
-      conf.password = database_url.password || "postgres"
-      # conf.port     = ENV["DATABASE_PORT"]? || 5342
+      conf.uri = ENV["DATABASE_URL"]
     end
   end
 end
