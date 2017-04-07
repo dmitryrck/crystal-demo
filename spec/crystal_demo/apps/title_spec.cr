33efe03dc9ccd547e05cd846bf96379d53f9c1ea
Dmitry Rocha <dmitryrck@gmail.com>
2017-04-07 10:59:42 -0300

    Add pagination to movies.

--- spec/crystal_demo/apps/title_spec.cr
+++ spec/crystal_demo/apps/title_spec.cr
@@ -20,7 +20,6 @@ describe CrystalDemo::TitleApp do
       title.title = "Passengers"
       title.kind_id = kind_type.instance.id
       title.production_year = 2016
-
       changeset = CrystalDemo::Title.changeset(title)
       CrystalDemo::Repo.insert(changeset)
 
@@ -39,7 +38,6 @@ describe CrystalDemo::TitleApp do
       title.title = "Passengers"
       title.kind_id = kind_type.instance.id
       title.production_year = 2016
-
       changeset = CrystalDemo::Title.changeset(title)
       CrystalDemo::Repo.insert(changeset)
 
@@ -50,6 +48,85 @@ describe CrystalDemo::TitleApp do
       end
 
       kind_types.should contain({ "name" => "episode" })
+    end
+  end
+
+  context "when paginating" do
+    context "should return first page" do
+      it "when has no page params" do
+        kind_type = CrystalDemo::KindType.new
+        kind_type.kind = "episode"
+        kind_type = CrystalDemo::KindType.changeset(kind_type)
+        kind_type = CrystalDemo::Repo.insert(kind_type)
+
+        1.upto(30).each do |number|
+          title = CrystalDemo::Title.new
+          title.title = "Movie##{number}"
+          title.kind_id = kind_type.instance.id
+          title.production_year = 2017
+          changeset = CrystalDemo::Title.changeset(title)
+          CrystalDemo::Repo.insert(changeset)
+        end
+
+        get "/titles"
+
+        JSON.parse(response.body).map { |movie| movie["title"] }.should eq(
+          [
+            "Movie#30", "Movie#29", "Movie#28", "Movie#27", "Movie#26", "Movie#25",
+            "Movie#24", "Movie#23", "Movie#22", "Movie#21"
+          ]
+        )
+      end
+
+      it "when page params is equals to 1" do
+        kind_type = CrystalDemo::KindType.new
+        kind_type.kind = "episode"
+        kind_type = CrystalDemo::KindType.changeset(kind_type)
+        kind_type = CrystalDemo::Repo.insert(kind_type)
+
+        1.upto(30).each do |number|
+          title = CrystalDemo::Title.new
+          title.title = "Movie##{number}"
+          title.kind_id = kind_type.instance.id
+          title.production_year = 2017
+          changeset = CrystalDemo::Title.changeset(title)
+          CrystalDemo::Repo.insert(changeset)
+        end
+
+        get "/titles?page=1"
+
+        JSON.parse(response.body).map { |movie| movie["title"] }.should eq(
+          [
+            "Movie#30", "Movie#29", "Movie#28", "Movie#27", "Movie#26", "Movie#25",
+            "Movie#24", "Movie#23", "Movie#22", "Movie#21"
+          ]
+        )
+      end
+    end
+
+    it "should return second page" do
+      kind_type = CrystalDemo::KindType.new
+      kind_type.kind = "episode"
+      kind_type = CrystalDemo::KindType.changeset(kind_type)
+      kind_type = CrystalDemo::Repo.insert(kind_type)
+
+      1.upto(30).each do |number|
+        title = CrystalDemo::Title.new
+        title.title = "Movie##{number}"
+        title.kind_id = kind_type.instance.id
+        title.production_year = 2017
+        changeset = CrystalDemo::Title.changeset(title)
+        CrystalDemo::Repo.insert(changeset)
+      end
+
+      get "/titles?page=2"
+
+      JSON.parse(response.body).map { |movie| movie["title"] }.should eq(
+        [
+          "Movie#20", "Movie#19", "Movie#18", "Movie#17", "Movie#16", "Movie#15",
+          "Movie#14", "Movie#13", "Movie#12", "Movie#11"
+        ]
+      )
     end
   end
 end
